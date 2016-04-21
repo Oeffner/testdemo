215cfe2aa1e1f0b7b80af995881e100a8091bbac
Robert Oeffner <rdo20@cam.ac.uk>
2016-04-21 11:34:03 +0100

    first commit

---
+++ utilfuncs.py
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      oeffner
+#
+# Created:     21/04/2016
+# Copyright:   (c) oeffner 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+
+
+
+
+def xfunc(F, sigF):
+  pass
+
+
+
+def yfunc(I, sigI):
+  pass
+
+
+def xyfunc(q, Z):
+  pass
